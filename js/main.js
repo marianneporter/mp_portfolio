8a06bef882d19bee7863c87a7196541f9aff4285
marianneporter <mariannneporter17@gmail.com>
2023-07-21 23:31:16 +0100

    WIP side nav added refactoring and added changes into about page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const mobileNavIcon = document.querySelector('.mobile-nav-icon');
-const closeNavBtn =  document.querySelector('.close-nav-btn')
+const closeNavBtn =  document.querySelector('.close-nav-btn');
+const mainContainer = document.querySelector('.main-container');
 const aside = document.querySelector('.aside');
+const overlay = document.querySelector('.overlay');
 
 /***************************************************************** */
 /*     code to run when page loaded                                */
@@ -8,7 +10,7 @@ const aside = document.querySelector('.aside');
 
 document.addEventListener("DOMContentLoaded", function() {
     setAsidePosition();
-    console.log('document loaded set aside position and close btn');
+   
 });
 
   
@@ -16,43 +18,28 @@ document.addEventListener("DOMContentLoaded", function() {
 /********************************************************************/
 /*   Event Listeners for menu functionality                         */ 
 /*********************************************************************/
-mobileNavIcon.addEventListener('click', () => {
-    console.log('mobile nav clicked');
-    // aside.classList.remove('aside-hidden');
-    // aside.classList.add('aside-active');
+mobileNavIcon.addEventListener('click', () => {  
     aside.style.left = "0";
-
+    overlay.style.opacity = "80%";
 });
 
 closeNavBtn.addEventListener('click', () => {
     console.log('close button clicked');
-    // aside.classList.remove('aside-active');
-    // aside.classList.add('aside-hidden');
     aside.style.left = "-15rem";
+    overlay.style.opacity = 0;
 });
 
 
 function setAsidePosition() {
 
-    if (window.innerWidth > 768) {
-        // aside.classList.remove('aside-hidden');
-        // aside.classList.add('aside-active');
-        // closeNavBtn.classList.remove('display');
-        // closeNavBtn.classList.add('no-display');  
-       
+    if (window.innerWidth > 768) {      
         aside.style.left = 0;
         closeNavBtn.style.display = 'none';
+        overlay.style.opacity = "0";
 
-    } else {
-        // aside.classList.remove('aside-active');
-        // aside.classList.add('aside-hidden');
-        // closeNavBtn.classList.remove('no-display');
-        // closeNavBtn.classList.add('display');
-        
+    } else { 
         aside.style.left = "-15rem";
-        closeNavBtn.style.display = 'block';
-
-        console.log('window > 768 adding diosplay tp class list');
+        closeNavBtn.style.display = 'block';        
     }
 }
 
@@ -60,6 +47,3 @@ window.addEventListener('resize', () => {
     console.log('resizing!')
     setAsidePosition();
 })
-
-
-
