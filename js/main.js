41c77c778929e49ffe76889ed24e12418c747695
marianneporter <mariannneporter17@gmail.com>
2023-07-21 18:00:52 +0100

    added window resizing in js to control display of menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 const mobileNavIcon = document.querySelector('.mobile-nav-icon');
+const aside = document.querySelector('.aside');
 
+
+/********************************************************************/
+/*   Event Listeners for menu functionality                         */ 
+/*********************************************************************/
 mobileNavIcon.addEventListener('click', () => {
     console.log('mobile nav clicked');
+  //  aside.classList.add('sm-aside-active');
+
 })
+
+
+function setAsidePosition() {
+
+    if (window.innerWidth > 768) {
+        aside.classList.remove('aside-hidden');
+        aside.classList.add('aside-active')
+    } else {
+        aside.classList.remove('aside-active');
+        aside.classList.add('aside-hidden');
+    }
+}
+
+window.addEventListener('resize', () => {
+    console.log('resizing!')
+    setAsidePosition();
+})
+
+
 
