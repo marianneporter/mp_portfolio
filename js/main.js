fada72b41522fbb4fad1f5a217dff22ca723b765
marianneporter <mariannneporter17@gmail.com>
2023-07-21 22:53:13 +0100

    added transition functionality for sliding  mobile menu

--- js/main.js
+++ js/main.js
@@ -18,31 +18,40 @@ document.addEventListener("DOMContentLoaded", function() {
 /*********************************************************************/
 mobileNavIcon.addEventListener('click', () => {
     console.log('mobile nav clicked');
-    aside.classList.remove('aside-hidden');
-    aside.classList.add('aside-active');
+    // aside.classList.remove('aside-hidden');
+    // aside.classList.add('aside-active');
+    aside.style.left = "0";
 
 });
 
 closeNavBtn.addEventListener('click', () => {
     console.log('close button clicked');
-    aside.classList.remove('.aside-active');
-    aside.classList.add('aside-hidden');
+    // aside.classList.remove('aside-active');
+    // aside.classList.add('aside-hidden');
+    aside.style.left = "-15rem";
 });
 
 
 function setAsidePosition() {
 
     if (window.innerWidth > 768) {
-        aside.classList.remove('aside-hidden');
-        aside.classList.add('aside-active');
-        closeNavBtn.classList.remove('display');
-        closeNavBtn.classList.add('no-display');  
+        // aside.classList.remove('aside-hidden');
+        // aside.classList.add('aside-active');
+        // closeNavBtn.classList.remove('display');
+        // closeNavBtn.classList.add('no-display');  
+       
+        aside.style.left = 0;
+        closeNavBtn.style.display = 'none';
 
     } else {
-        aside.classList.remove('aside-active');
-        aside.classList.add('aside-hidden');
-        closeNavBtn.classList.remove('no-display');
-        closeNavBtn.classList.add('display');
+        // aside.classList.remove('aside-active');
+        // aside.classList.add('aside-hidden');
+        // closeNavBtn.classList.remove('no-display');
+        // closeNavBtn.classList.add('display');
+        
+        aside.style.left = "-15rem";
+        closeNavBtn.style.display = 'block';
+
         console.log('window > 768 adding diosplay tp class list');
     }
 }
