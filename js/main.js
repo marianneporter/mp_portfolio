ecdb8ba2bb39ab2f1ba5de1a21827069790a0db7
marianneporter <mariannneporter17@gmail.com>
2023-07-21 18:52:09 +0100

    nav displaying in both small and large modes as required needs css modifications and animations next

--- js/main.js
+++ js/main.js
@@ -1,25 +1,49 @@
 const mobileNavIcon = document.querySelector('.mobile-nav-icon');
+const closeNavBtn =  document.querySelector('.close-nav-btn')
 const aside = document.querySelector('.aside');
 
+/***************************************************************** */
+/*     code to run when page loaded                                */
+/***************************************************************** */
+
+document.addEventListener("DOMContentLoaded", function() {
+    setAsidePosition();
+    console.log('document loaded set aside position and close btn');
+});
+
+  
 
 /********************************************************************/
 /*   Event Listeners for menu functionality                         */ 
 /*********************************************************************/
 mobileNavIcon.addEventListener('click', () => {
     console.log('mobile nav clicked');
-  //  aside.classList.add('sm-aside-active');
+    aside.classList.remove('aside-hidden');
+    aside.classList.add('aside-active');
 
-})
+});
+
+closeNavBtn.addEventListener('click', () => {
+    console.log('close button clicked');
+    aside.classList.remove('.aside-active');
+    aside.classList.add('aside-hidden');
+});
 
 
 function setAsidePosition() {
 
     if (window.innerWidth > 768) {
         aside.classList.remove('aside-hidden');
-        aside.classList.add('aside-active')
+        aside.classList.add('aside-active');
+        closeNavBtn.classList.remove('display');
+        closeNavBtn.classList.add('no-display');  
+
     } else {
         aside.classList.remove('aside-active');
         aside.classList.add('aside-hidden');
+        closeNavBtn.classList.remove('no-display');
+        closeNavBtn.classList.add('display');
+        console.log('window > 768 adding diosplay tp class list');
     }
 }
 
