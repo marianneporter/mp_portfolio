7b8923f0845747e4d991510e59d9b2f6f62a1722
marianneporter <mariannneporter17@gmail.com>
2023-07-25 21:28:59 +0100

    wip code samples add slick slider

--- js/coding.js
+++ js/coding.js
@@ -1,7 +1,20 @@
-$('.coding-slides').slick({ 
+$('.coding-slides').slick({
+    dots: true,
+    infinite: true,  
     slidesToShow: 1,
-    slidersToScroll: 1,
-    speed: 300,
+    slidesToScroll: 1,
     arrows: true,
-    dots: true,    
-});
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false
+        }
+      }  
+    ]
+  });
+              
