bf132a37da66bc5d0e3b02f54922132f2436c5b3
marianneporter <mariannneporter17@gmail.com>
2023-07-27 17:23:56 +0100

    multiple small issues, contact form confirmation message, suspend animations on initial page load

--- js/common.js
+++ js/common.js
@@ -1,17 +1,23 @@
-// prevent all animations if window is in the process of resizing or initialisation
-// not complete
-let resizeTimer;
-document.body.classList.add("resize-transition-stopper");
-document.addEventListener('DOMContentLoaded', () => {
-    document.body.classList.remove("resize-transition-stopper"); 
+/**************************************************************/
+/* stop all transitions when page first loaded and on page    */
+/* resize to maintain consistent layout                       */
+/**************************************************************/    
+
+let transitionStopTimer;
+
+window.addEventListener("DOMContentLoaded", () => {       
+    document.body.classList.add("transition-stopper");
+    clearTimeout(transitionStopTimer);
+    transitionStopTimer = setTimeout(() => {
+    document.body.classList.remove("transition-stopper"); 
+  }, 400);
 });
 
-
 window.addEventListener("resize", () => {   
-    document.body.classList.add("resize-transition-stopper");
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-    document.body.classList.remove("resize-transition-stopper"); 
+    document.body.classList.add("transition-stopper");
+    clearTimeout(transitionStopTimer);
+    transitionStopTimer = setTimeout(() => {
+    document.body.classList.remove("transition-stopper"); 
   }, 400);
 });
 
