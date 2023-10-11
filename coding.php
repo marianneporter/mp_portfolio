2c34a7a010c23ffbf609c4cb7214395f44f07a5d
marianneporter <marianneporter17@gmail.com>
2023-10-11 17:55:35 +0100

    issues with header amd nav, now refactored into php include files

--- coding.php
+++ coding.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="slick/slick.css">    
-    <link rel="stylesheet" href="slick/slick-theme.css">      
-    <link rel="stylesheet" href="css/index.css">
-    <title>Coding Examples</title>
-</head>
+<?php require_once 'includes/header.php'; ?>  
+
 <body>
 
     <div class="outer-container">
---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<?php 
+    $currentPage = basename($_SERVER['PHP_SELF']);
+   
+    if ($currentPage == "coding.php") {
+        $title = "Coding Examples";
+    } else if ($currentPage == "about.php")  {
+        $title = "About Me";
+    } else if ($currentPage == "scs.php") {
+        $title = "SCS Scheme";
+    } else {
+        $title = "Portfolio";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <?php if($currentPage == "coding.php") { ?>
+        <link rel="stylesheet" href="slick/slick.css">    
+        <link rel="stylesheet" href="slick/slick-theme.css">  
+    <?php } ?>
+
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="icon" href="favicon.ico" type="image/ico">  
+    
+    <title> <?php echo $title ?></title>
+</head>
